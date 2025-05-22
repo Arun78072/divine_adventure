cd469bdbefe08ab57aec20de0ecc4a767c6f80a5
Arun <51883589+Arun78072@users.noreply.github.com>
2025-05-22 21:44:05 +0530

    new change

--- pages/destination/index.js
+++ pages/destination/index.js
@@ -1,4 +1,4 @@
-import { CiFilter } from "react-icons/ci";
+import { CiCalendarDate, CiFilter } from "react-icons/ci";
 import { useEffect, useState } from "react";
 import { baseUrl } from "@/utils";
 import axios from "axios";
@@ -10,45 +10,87 @@ import Loader from "@/components/Loader";
 import CreateEditSpark from "@/components/Form/CreateEditSpark";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import { IoLocationOutline } from "react-icons/io5";
+import { FaRupeeSign } from "react-icons/fa";
 
 export default function AllSparks() {
-  const [searchQuery, setSearchQuery] = useState(undefined);
   const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState([]);
   const [activeScreen, setActiveScreen] = useState("my_spark");
   const [editFormData, setEditFormData] = useState({});
-  const [searchError, setSearchError] = useState("");
-  const { query } = useRouter();
-  const router = useRouter();
-
-
 
   return (
-    <main className="p-3">
+    <main className="destination_section">
       <Loader loading={loading} />
-    
-      {activeScreen === "my_spark" ? (
-        <section className="w-full mt-32 my-5">
-          <div className="max-w-screen-lg mx-auto min-h-[60vh]">
-            <h1 className="font-semibold ml-6 text-3xl">All Tours Card </h1>
-            <Link
-              className="red-500 duration-200"
-              href="/destination/view/00000"
-            >
-              Single Tour
-            </Link>
-            
-             <Image
-         src="/assets/alltour.png"
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{ width: "100%", height: "auto" }} 
-      />
-            <p className="text-red-500 ml-2">{searchError}</p>
-         
+
+      <section className="container">
+        <div className="max-w-screen-lg mx-auto min-h-[60vh]">
+          <h1 className="font-semibold ml-6 text-3xl">All Tours Card </h1>
+          <Link className="red-500 duration-200" href="/destination/view/00000">
+            Single Tour
+          </Link>
+
+          {/* Filter Bar  */}
+          <h1>Filter Bar</h1>
+
+          {/* All Tour Post */}
+          <div className="post_grid">
+            {[1, 1, 1, 1, 1, 1]?.map((item, index) => {
+              return (
+                <div className="post_card" key={index}>
+                  <div className="image_wrapper">
+                    <Image
+                      src={
+                        "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                      }
+                      alt={"title"}
+                      width={300}
+                      height={300}
+                      className="post_image"
+                    />
+                  </div>
+                  <div className="info-row">
+                    <span className="icon">
+                      <CiCalendarDate /> 8 Days
+                    </span>
+                    <span className="icon">
+                      <IoLocationOutline /> 3 Locations
+                    </span>
+                  </div>
+                  <Link
+                    href={`/posts/view/${item?._id}`}
+                    className="post_title"
+                  >
+                    Switzerland
+                  </Link>
+                  <div className="location_row">
+                    <IoLocationOutline />
+                    India
+                  </div>
+                  <h3 className="price">
+                    <FaRupeeSign /> 12000
+                  </h3>
+                  <p className="post_description">
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
+                  <div className="action_buttons">
+                    <button className="secandary_button">Query Form</button>
+                    <button className="primary_button">Explore Now</button>
+                  </div>
+                </div>
+              );
+            })}
           </div>
-        </section>
+
+          {/* Pagination */}
+          <div className="">Pagination bar</div>
+        </div>
+      </section>
+
+      {activeScreen === "my_spark" ? (
+        "ghhhh"
       ) : activeScreen === "add_spark" ? (
         <CreateEditSpark
           data={editFormData}
