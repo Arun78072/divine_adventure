c727ca02263a2734eaf369dba65859bec89040c7
Arun <51883589+Arun78072@users.noreply.github.com>
2025-05-31 13:19:58 +0530

    new chnages in info tab

--- components/Form/CreateEditSpark.js
+++ components/Form/CreateEditSpark.js
@@ -7,33 +7,7 @@ import Loader from "../Loader";
 export default function CreateEditTour({ data }) {
   const [loading, setLoading] = useState(false);
   const [editFormId, setEditFormId] = useState();
-  const [formData, setFormData] = useState({
-    title: "Tour Name",
-    tourImage:
-      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.treebo.com%2Fblog%2Fmasroor-temple%2F&psig=AOvVaw1v97qnmDM2s1HdswthVh_2&ust=1748253427043000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPixjNitvo0DFQAAAAAdAAAAABAE",
-    status: "PUBLIC",
-    tourInfo: {
-      title: "Masroor Mandir ",
-      description: "This is masroor mandir okay ",
-      price: 20000,
-      destination: "masoor",
-      depature: "12 / 10 / ",
-      include: ["sb kuch"],
-      travelDays: "5",
-      travelCountry: "1",
-      travelCity: "3",
-      travelNight: "6",
-    },
-    tourPlan: [
-      {
-        title: "tour title",
-        description: "tour description ",
-        list: [""],
-      },
-    ],
-    location:
-      '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4085.217993504921!2d76.13469897630256!3d32.07276297396586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391b43b22d895a9d%3A0x78c1bd87f3285412!2sMasroor%20Rock-cut%20Temple!5e1!3m2!1sen!2sin!4v1748167152928!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
-  });
+  const [formData, setFormData] = useState({});
 
   const SubmitTour = async () => {
     try {
@@ -56,7 +30,7 @@ export default function CreateEditTour({ data }) {
       setFormData(data);
     }
   }, [data]);
-
+console.log('formData======>',formData)
   return (
     <div className="form_box">
       <Loader loading={loading} />
@@ -103,7 +77,7 @@ export default function CreateEditTour({ data }) {
                 <label>Tour Info Title</label>
                 <input
                   type="text"
-                  value={formData.tourInfo.title}
+                  value={formData?.tourInfo?.title}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -115,7 +89,7 @@ export default function CreateEditTour({ data }) {
               <div>
                 <label>Description</label>
                 <textarea
-                  value={formData.tourInfo.description}
+                  value={formData?.tourInfo?.description}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -131,7 +105,7 @@ export default function CreateEditTour({ data }) {
                 <label>Price</label>
                 <input
                   type="number"
-                  value={formData.tourInfo.price}
+                  value={formData?.tourInfo?.price}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -147,7 +121,7 @@ export default function CreateEditTour({ data }) {
                 <label>Destination</label>
                 <input
                   type="text"
-                  value={formData.tourInfo.destination}
+                  value={formData?.tourInfo?.destination}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -163,7 +137,7 @@ export default function CreateEditTour({ data }) {
                 <label>Depature</label>
                 <input
                   type="text"
-                  value={formData.tourInfo.depature}
+                  value={formData?.tourInfo?.depature}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -179,7 +153,7 @@ export default function CreateEditTour({ data }) {
                 <label>Include In Tour Plan </label>
                 <input
                   type="text"
-                  value={formData.tourInfo.include[0]}
+                  value={formData?.tourInfo?.include[0]}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -195,7 +169,7 @@ export default function CreateEditTour({ data }) {
                 <label>Travel Days</label>
                 <input
                   type="text"
-                  value={formData.tourInfo.travelDays}
+                  value={formData?.tourInfo?.travelDays}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -211,7 +185,7 @@ export default function CreateEditTour({ data }) {
                 <label>Travel Country</label>
                 <input
                   type="text"
-                  value={formData.tourInfo.travelCountry}
+                  value={formData?.tourInfo?.travelCountry}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -227,7 +201,7 @@ export default function CreateEditTour({ data }) {
                 <label>Travel City</label>
                 <input
                   type="text"
-                  value={formData.tourInfo.travelCity}
+                  value={formData?.tourInfo?.travelCity}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
@@ -243,7 +217,7 @@ export default function CreateEditTour({ data }) {
                 <label>Travel Night</label>
                 <input
                   type="text"
-                  value={formData.tourInfo.travelNight}
+                  value={formData?.tourInfo?.travelNight}
                   onChange={(e) =>
                     setFormData({
                       ...formData,
--- pages/destination/edit/[destinationSlug].js
+++ pages/destination/edit/[destinationSlug].js
@@ -42,7 +42,6 @@ export default function EditPost() {
   return (
     <div>
       <Loader loading={loading} />
-
       <CreateEditSpark data={postData} />
     </div>
   );
