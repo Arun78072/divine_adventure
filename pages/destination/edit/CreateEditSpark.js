808871cdb803a786d7bf2f0258a89512fb42100f
Arun <51883589+Arun78072@users.noreply.github.com>
2025-06-07 01:14:38 +0530

    again check

--- pages/destination/edit/CreateEditSpark.js
+++ pages/destination/edit/CreateEditSpark.js
@@ -794,8 +794,6 @@ export default function CreateEditTour({ data }) {
                 </div>
                 </div> */}
 
-                {/* new/ */}
-
               </div>
 
               <button onClick={SubmitTour} className="primary_button">
