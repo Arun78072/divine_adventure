f7ea7d9fcef5c6a25ff1044fed53b9c67e1b8fd6
Arun <51883589+Arun78072@users.noreply.github.com>
2025-07-08 23:35:16 +0530

    changes

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const baseUrl = "https://divine-adventure.vercel.app/";
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://divine-adventure.vercel.app/";
+// export const baseUrl = "http://localhost:3000";
 
 export const tourTypeOption = [
   {
